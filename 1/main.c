d45c5a1c3aab09be7a2273e17f41f22a221ffd57
Christopher Abreu <cfa911@outlook.pt>
2019-03-21 14:21:44 +0000

    File description working w/o hashing

--- 1/main.c
+++ 1/main.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <string.h>
+#include <time.h>
 
-int main(int argc, int *argv[], int *envp[])
+int main(int argc, int **argv, int *envp[])
 {
     int fileIndex = argc - 1;
     FILE *pf;
-
     struct stat fileStat;
-    if (stat(argv[1], &fileStat) < 0)
+    if (stat((char *)argv[fileIndex], &fileStat) < 0)
         return 1;
 
-    char command[20] = "file ";
+    char command[512] = "file ";
     //file_name,file_type,file_size,file_access,file_created_date,file_modification_date,md5,sha1,sha256
 
-    strcat(command, argv[fileIndex]);
+    strcat(command, (char *)argv[fileIndex]);
 
     // Setup our pipe for reading and execute our command.
     pf = popen(command, "r");
@@ -24,37 +25,40 @@ int main(int argc, int *argv[], int *envp[])
 
     // Get the data from the process execution
     fgets(data, 512, pf);
+    
+    if (stat((char *)argv[fileIndex], &fileStat) < 0)
+        return 1;
 
-    printf(data);
+    printf("Information for %s\n", (char *)argv[fileIndex]);
+    
+    printf("---------------------------\n");
+    //puts(data); 
+    fputs(data,stdout);
+    write(STDOUT_FILENO,"Filesize: ",11);
+    printf("%d bytes\n", (int)fileStat.st_size);
 
-    printf((S_ISDIR(fileStat.st_mode)) ? "d" : "-");
-    printf((fileStat.st_mode & S_IRUSR) ? "r" : "-");
-    printf((fileStat.st_mode & S_IWUSR) ? "w" : "-");
-    printf((fileStat.st_mode & S_IXUSR) ? "x" : "-");
-    printf((fileStat.st_mode & S_IRGRP) ? "r" : "-");
-    printf((fileStat.st_mode & S_IWGRP) ? "w" : "-");
-    printf((fileStat.st_mode & S_IXGRP) ? "x" : "-");
-    printf((fileStat.st_mode & S_IROTH) ? "r" : "-");
-    printf((fileStat.st_mode & S_IWOTH) ? "w" : "-");
-    printf((fileStat.st_mode & S_IXOTH) ? "x" : "-");
-    /*
-    switch (bits)
-    {
+    write(STDOUT_FILENO,(S_ISDIR(fileStat.st_mode)) ? "d" : "-",2);
+    write(STDOUT_FILENO,(fileStat.st_mode & S_IRUSR) ? "r" : "-",2);
+    write(STDOUT_FILENO,(fileStat.st_mode & S_IWUSR) ? "w" : "-",2);
+    write(STDOUT_FILENO,(fileStat.st_mode & S_IXUSR) ? "x" : "-",2);
+    write(STDOUT_FILENO,(fileStat.st_mode & S_IRGRP) ? "r" : "-",2);
+    write(STDOUT_FILENO,(fileStat.st_mode & S_IWGRP) ? "w" : "-",2);
+    write(STDOUT_FILENO,(fileStat.st_mode & S_IXGRP) ? "x" : "-",2);
+    write(STDOUT_FILENO,(fileStat.st_mode & S_IROTH) ? "r" : "-",2);
+    write(STDOUT_FILENO,(fileStat.st_mode & S_IWOTH) ? "w" : "-",2);
+    write(STDOUT_FILENO,(fileStat.st_mode & S_IXOTH) ? "x" : "-",2);
+    char str[32];
+    time_t rawtime;
+    struct tm *crtTime;
+    struct tm *modTime;
 
-    case S_IRWXU: // owner has read, write, and execute permission
-    case S_IRUSR: // owner has read permission
-    case S_IWUSR: // owner has write permission
-    case S_IXUSR: // owner has execute permission
-    case S_IRWXG: // group has read, write, and execute permission
-    case S_IRGRP: // group has read permission
-    case S_IWGRP: // group has write permission
-    case S_IXGRP: // group has execute permission
-    case S_IRWXO: // others (not in group) have read, write, and execute permission
-    case S_IROTH: // others have read permission
-    case S_IWOTH: // others have write permission
-    case S_IXOTH: // others have execute permission
-        break;
-    }*/
+    crtTime = localtime(&fileStat.st_ctime);
+    modTime = localtime(&fileStat.st_mtime);
+
+    printf("\nModification Date: %d-%d-%dT%d:%d:%d", 1900 + modTime->tm_year,modTime->tm_mon,modTime->tm_mday,modTime->tm_hour,modTime->tm_min,modTime->tm_sec);
+    printf("\nCreation Date: %d-%d-%dT%d:%d:%d", 1900 + crtTime->tm_year,crtTime->tm_mon,crtTime->tm_mday,crtTime->tm_hour,crtTime->tm_min,crtTime->tm_sec);
+
+    printf("\n---------------------------\n");
 
     return 0;
 }
